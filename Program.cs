01fd635a999e2f45aa7ea6e54563e5310d595670
ZavarkoZeno <zavarkoz@kkszki.hu>
2024-10-11 12:58:06 +0200

    Végeztem a projekttel

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            HashSet<int> lottóSzámok = new HashSet<int>();
+            int számokSzáma = 6;
+            int maxSzám = 49;
+
+            while (lottóSzámok.Count < számokSzáma)
+            {
+                int újSzám = random.Next(1, maxSzám + 1);
+                lottóSzámok.Add(újSzám);
+            }
+
+            Console.WriteLine("A lottó sorsolás számai:");
+            foreach (int szám in lottóSzámok)
+            {
+                Console.Write(szám + " ");
+            }
+        }
+    }
+}
